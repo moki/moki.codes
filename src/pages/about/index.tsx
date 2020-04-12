f0baca3462bfa47bc6c23ffb441d311756d095a1
Kirill Morozov (moki) <morozov.kirill.moki@gmail.com>
2020-04-12 20:03:27 +0300

    feat(pages): refactor templates

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,11 +1,14 @@
 import { List, strings } from "@moki.codes/mokui-list";
-import { VIRTUAL_DOM_RENDER_FINISH } from "../../../lib/render";
+/*import { VIRTUAL_DOM_RENDER_FINISH } from "../../../lib/render";*/
 
 import "@moki.codes/mokui-card/dist/mokui-card.css";
 import "@moki.codes/mokui-list/dist/mokui-list.css";
 
+import { Snackbar } from "../../components/snackbar";
+
 import "../../components/separator/styles.css";
 import "../../components/section/styles.css";
+import "../../components/snackbar/styles.css";
 
 import "./styles.css";
 import "./section-hero.css";
@@ -19,92 +22,7 @@ import "./info-contacts.css";
 
 let contactsList: any;
 let socialsList: any;
-let snackbar: Element | null;
 let emailId: string;
-
-function Snackbar() {
-        if (snackbar) return;
-        snackbar = document.createElement("div");
-        const snackbarClasses = ["snackbar"];
-        snackbar.classList.add(...snackbarClasses);
-
-        const container = document.createElement("div");
-        const containerClasses = ["snackbar__container"];
-        container.classList.add(...containerClasses);
-
-        const content = document.createElement("div");
-        const contentClasses = [
-                "snackbar__content",
-                "elevation",
-                "elevation_depth_6"
-        ];
-        content.classList.add(...contentClasses);
-
-        const text = document.createElement("div");
-        const textClasses = [
-                "text_line-height_m",
-                "text_size_s",
-                "text_weight_medium",
-                "text_style_small-caps",
-                "text_letter-spacing_m"
-        ];
-        text.classList.add(...textClasses);
-        text.innerHTML = "email copied to the clipboard";
-
-        const action = document.createElement("div");
-        const actionClasses = ["snackbar__action"];
-        action.classList.add(...actionClasses);
-        action.innerHTML =
-                '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/><path d="M0 0h24v24H0z" fill="none"/></svg>';
-
-        const close = action.querySelector("svg");
-        if (!close) return;
-
-        let tick = false;
-        let target = 200;
-        let animating = false;
-        let start: number;
-
-        function closeTick() {
-                requestAnimationFrame(closeAnimation);
-                tick = true;
-        }
-
-        function closeAnimation(time: number) {
-                let fraction = (time - start) / 300;
-                fraction = fraction > 1 ? 1 : fraction;
-                let progress = 1 - Math.sin(Math.acos(fraction));
-                snackbar!.setAttribute(
-                        "style",
-                        `transform: translate(-50%, ${target * progress}%);
-                                `
-                );
-
-                if (fraction < 1) closeTick();
-                else {
-                        document.body.removeChild(snackbar!);
-                        snackbar = null;
-                }
-        }
-
-        close.addEventListener("click", e => {
-                animating = true;
-                start = performance.now();
-                closeTick();
-        });
-
-        content.appendChild(text);
-        content.appendChild(action);
-        container.appendChild(content);
-        snackbar.appendChild(container);
-        document.body.appendChild(snackbar);
-
-        setTimeout(() => {
-                if (animating) return;
-                start = performance.now();
-                closeTick();
-        }, 3000);
-}
 
 const copyMail = (e: Event) => {
         const element = e.target as Element;
@@ -127,7 +45,11 @@ const copyMail = (e: Event) => {
         fakeinput.blur();
         element.removeChild(fakeinput);
 
-        Snackbar();
+        Snackbar({
+                message: "email copied to the clipboard",
+                timeout: 3000,
+                hidems: 350
+        });
 };
 
 export const load = () => {
@@ -154,7 +76,7 @@ export const unload = () => {
 
 if (process.env.NODE_ENV === "development") {
         if (window.location.pathname === "/about") {
-                window.addEventListener(VIRTUAL_DOM_RENDER_FINISH, load);
+                window.addEventListener("load", load);
                 window.addEventListener("unload", unload);
         }
 } else {
--- src/pages/code/index.tsx
+++ src/pages/code/index.tsx
@@ -4,3 +4,13 @@ import "./section-hero.css";
 
 export const load = () => {};
 export const unload = () => {};
+
+if (process.env.NODE_ENV === "development") {
+        if (window.location.pathname === "/code") {
+                window.addEventListener("load", load);
+                window.addEventListener("unload", unload);
+        }
+} else {
+        window.addEventListener("load", load);
+        window.addEventListener("unload", unload);
+}
