b7409bc48ea25ab96fad5ef8a7e8b12682214e44
Kirill Morozov (moki) <morozov.kirill.moki@gmail.com>
2020-04-13 08:49:41 +0300

    feat(sub-form): implement component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import "@moki.codes/mokui-card/dist/mokui-card.css";
 import "@moki.codes/mokui-button/dist/mokui-button.css";
 
 import { TextField, strings } from "../components/textfield";
+import { SubscribeForm } from "./subscribe-form";
 
 import "../components/section/styles.css";
 import "../components/article-preview/styles.css";
@@ -11,51 +12,11 @@ import "./section-hero.css";
 import "./section-articles.css";
 import "./section-code.css";
 import "./section-subscribe.css";
+import "./subscribe-form.css";
 
 let nameField: TextField;
 let emailField: TextField;
-let formSubmit: Element | null;
-let form: Element | null;
-let kitty: Element | null;
-
-let state = {
-        isKittyTyping: false,
-        formDirty: false
-};
-
-const stateValidChanged = (e: Event) => {
-        if (emailField.valid && nameField.valid) {
-                formSubmit!.classList.remove("button_disabled");
-                form!.classList.add("section-subscribe__form_valid");
-                form!.classList.remove("section-subscribe__form_invalid");
-        } else {
-                formSubmit!.classList.add("button_disabled");
-                form!.classList.add("section-subscribe__form_invalid");
-                form!.classList.remove("section-subscribe__form_valid");
-        }
-};
-
-const formFocusinHandler = (e: Event) => {
-        const element = e.target! as HTMLInputElement;
-        if (element !== nameField.input && element !== emailField.input) return;
-        state.isKittyTyping = true;
-        kitty!.classList.add("kitty_types");
-};
-
-const formFocusoutHandler = (e: Event) => {
-        const element = e.target! as HTMLInputElement;
-        if (element !== nameField.input && element !== emailField.input) return;
-        state.isKittyTyping = false;
-        kitty!.classList.remove("kitty_types");
-};
-
-const formInputHandler = (e: Event) => {
-        const element = e.target! as HTMLInputElement;
-        if (element !== nameField.input && element !== emailField.input) return;
-        if (state.formDirty) return;
-        state.formDirty = true;
-        stateValidChanged(e);
-};
+let subform: SubscribeForm;
 
 export const load = () => {
         nameField = TextField({
@@ -70,34 +31,23 @@ export const load = () => {
                 labelSelectedClasses: ["elevation", "elevation_depth_2"]
         });
 
-        formSubmit = document.querySelector(".section-subscribe-form__submit");
-        if (!formSubmit) throw new Error("didn't find form submit button");
+        const submit = document.querySelector(".subscribe-form__submit");
+        if (!submit) throw new Error("Failed");
+        const kitty = document.querySelector(".kitty");
+        if (!kitty) throw new Error("Failed");
 
-        form = document.querySelector("#subform");
-        if (!form) throw new Error("didn't find form title");
-
-        kitty = document.querySelector(".kitty");
-        if (!kitty) throw new Error("didn't find kitty");
-
-        window.addEventListener(
-                strings.STATE_VALID_CHANGED_EVENT,
-                stateValidChanged
-        );
-
-        window.addEventListener("focusin", formFocusinHandler);
-        window.addEventListener("focusout", formFocusoutHandler);
-        window.addEventListener("input", formInputHandler);
+        subform = SubscribeForm({
+                id: "subform",
+                fields: [nameField, emailField],
+                submit,
+                kitty
+        });
 };
 
 export const unload = () => {
         nameField.destroy();
         emailField.destroy();
-        window.removeEventListener(
-                strings.STATE_VALID_CHANGED_EVENT,
-                stateValidChanged
-        );
-        window.removeEventListener("focusin", formFocusinHandler);
-        window.removeEventListener("focusout", formFocusoutHandler);
+        subform.destroy();
 };
 
 if (process.env.NODE_ENV === "development") {
--- src/pages/section-subscribe.tsx
+++ src/pages/section-subscribe.tsx
@@ -9,7 +9,7 @@ import {
 } from "../components/card";
 import { Container } from "../components/container";
 import { Section } from "../components/section";
-import { ButtonGhost } from "../components/button";
+import { SubscribeFormTemplate as SubscribeForm } from "./subscribe-form";
 
 const Header = ({ children }: { children?: any }) => (
         <h2 class="text_line-height_xl text_size_4xl text_weight_xbold section-subscribe__title">
@@ -23,102 +23,14 @@ const Subheader = ({ children }: { children?: any }) => (
         </h3>
 );
 
-const TextField = ({
-        type,
-        id,
-        label,
-        elevation
-}: {
-        type: string;
-        id: string;
-        label: string;
-        elevation: number;
-}) => {
-        const inputClasses = `text-field__input text_line-height_m text_size_s text_weight_regular text_style_small-caps text_letter-spacing_m elevation elevation_depth_${elevation}`;
-        const labelClasses =
-                "text-field__label text_line-height_m text_size_s text_weight_regular text_style_small-caps text_letter-spacing_m";
-        return (
-                <div class="text-field">
-                        <input type="text" id={id} class={inputClasses} />
-                        <label for={id} class={labelClasses}>
-                                {label}
-                        </label>
-                </div>
-        );
-};
-const Form = ({
-        id,
-        autocomplete,
-        children
-}: {
-        id: string;
-        autocomplete: "on" | "off";
-        children?: any;
-}) => {
-        const formClasses = "section-subscribe__form";
-        return (
-                <form class={formClasses} id={id} autocomplete={autocomplete}>
-                        {children}
-                </form>
-        );
-};
-
-const FormTitle = ({ children }: { children?: any }) => {
-        const classes =
-                "text_line-height_m text_size_xl text_weight_medium section-subscribe-form__title kitty";
-        return <div class={classes}>{children}</div>;
-};
-
-const FormSubmit = ({ children }: { children?: any }) => {
-        const classes = "section-subscribe-form__submit-wrapper";
-        return <div class={classes}>{children}</div>;
-};
-
 export const Subscribe = () => {
-        const submitClasses =
-                "text_line-height_m text_style_small-caps text_letter-spacing_m text_size_m text_weight_medium section-subscribe-form__submit button_disabled";
         return (
                 <Section vpadding="l" classes="section-subscribe">
                         <Container classes="subscribe">
                                 <Header>Stay Tuned</Header>
                                 <Subheader>Subscribe for updates</Subheader>
                                 <Card elevation={1}>
-                                        <Form id="subform" autocomplete="off">
-                                                <FormTitle>
-                                                        <span class="kitty__paw-left">
-                                                                つ
-                                                        </span>
-                                                        &#8291; &#8291; &#8291;
-                                                        &#8291; ^•ﻌ•^
-                                                        <span class="kitty__paw-right">
-                                                                つ
-                                                        </span>
-                                                </FormTitle>
-                                                <TextField
-                                                        type="text"
-                                                        id="name"
-                                                        label="name"
-                                                        elevation={2}
-                                                />
-                                                <TextField
-                                                        type="email"
-                                                        id="email"
-                                                        label="email"
-                                                        elevation={2}
-                                                />
-                                                <FormSubmit>
-                                                        <ButtonGhost
-                                                                size="l"
-                                                                color="primary-light"
-                                                                elevation={2}
-                                                                classes={
-                                                                        submitClasses
-                                                                }
-                                                        >
-                                                                Subscribe
-                                                        </ButtonGhost>
-                                                </FormSubmit>
-                                        </Form>
+                                        <SubscribeForm elevation={1} />
                                 </Card>
                         </Container>
                 </Section>
