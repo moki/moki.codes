b08e31886088c1137245dd5fcba7b3eb5c1b8e68
Kirill Morozov (moki) <morozov.kirill.moki@gmail.com>
2020-05-22 03:10:03 +0300

    feat(app): root

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,100 @@
-import { render, h, Component, Fragment } from "preact";
+import { render, h, Fragment } from "preact";
+import { useState } from "preact/hooks";
+import { useLocation } from "wouter-preact";
 
-function inlineStyle(style: string) {
-        document.head.innerHTML += "<style>" + style + "</style>";
+import { insertStyles } from "lib/insertStyles";
+import { resolveDefault } from "lib/resolveDefault";
+
+import { Header } from "src/components/header";
+import { Drawer } from "src/components/drawer";
+import { Main } from "src/components/main";
+import { Footer } from "src/components/footer";
+import { Snackbar } from "src/components/snackbar";
+
+import { Theme } from "src/components/theme";
+
+import { Sun, Moon, Menu } from "src/components/icons";
+
+import { Home } from "src/pages/home";
+import { Articles } from "src/pages/articles";
+import { Code } from "src/pages/code";
+import { About } from "src/pages/about";
+
+const routes = [
+        { name: "home", url: "/" },
+        { name: "about", url: "/about" },
+        { name: "articles", url: "/articles" },
+        { name: "code", url: "/code" }
+];
+
+const pages: { [key: string]: () => preact.JSX.Element } = {
+        home: Home,
+        about: About,
+        articles: Articles,
+        code: Code,
+        notfound: () => <div>not found</div>
+};
+
+function Router({ location }: { location: string }) {
+        const route = routes.filter(e => e.url === location);
+        if (route.length) return pages[route[0].name];
+        else return pages["notfound"];
 }
 
-[import("src/styles.css"), import("lib/reset.css")].forEach(i =>
-        i.then(m => inlineStyle(m.default))
-);
+function App() {
+        const theme = Theme();
+        const [location, setLocation] = useLocation();
+        const [drawer, setDrawer] = useState(false);
+        const classes = [...theme.classes, "layout", "layout_hg"].join(" ");
 
-class App extends Component {
-        state = { loaded: false };
+        const primaryAction = <Menu onClick={() => setDrawer(!drawer)} />;
+        const secondaryAction = theme.color ? (
+                <Sun onClick={theme.toggleColor} />
+        ) : (
+                <Moon onClick={theme.toggleColor} />
+        );
 
-        render(...args: any[]) {
-                console.log(args);
-                return (
-                        <Fragment>
-                                <div class="form">
-                                        <h2>app</h2>
-                                        <p>text</p>
-                                </div>
-                        </Fragment>
-                );
-        }
+        const Route = Router({ location });
+
+        return (
+                <div class={classes} style="grid-template-rows: 56px 1fr auto;">
+                        <Snackbar>
+                                <Header
+                                        classes="layout_hg__header header_fixed"
+                                        primaryAction={primaryAction}
+                                        secondaryAction={secondaryAction}
+                                        routes={routes}
+                                ></Header>
+                                <Drawer
+                                        open={drawer}
+                                        close={setDrawer}
+                                        routes={routes}
+                                />
+                                <Route />
+                                <Footer
+                                        classes="layout_hg__footer"
+                                        routes={routes}
+                                >
+                                        <div>footer</div>
+                                </Footer>
+                        </Snackbar>
+                </div>
+        );
 }
+
+Promise.all(
+        [
+                import("lib/reset.css"),
+                import("src/components/theme/styles.css"),
+                import("src/components/elevation/styles.css"),
+                import("src/components/layout/styles.css"),
+                import("src/components/grid/styles.css"),
+                import("src/components/header/styles.css"),
+                import("src/components/footer/styles.css"),
+                import("src/components/drawer/styles.css"),
+                import("src/components/snackbar/styles.css"),
+                import("src/styles.css")
+        ].map(resolveDefault)
+).then(insertStyles);
 
 render(<App />, document.body);
